d3037a8e6e03a84ea00acd91372552453ecd19ac
邵一凡 <99520992+ShawWoods@users.noreply.github.com>
2025-03-20 16:48:44 +0800

    Delete app/api/test directory

--- app/api/test/route.js
+++
@@ -1,9 +0,0 @@
-// app/api/test/route.js
-export async function GET(request) {
-    console.log('测试路由，方法: GET');
-    const url = request.nextUrl.searchParams.get('url'); // 获取查询参数
-    return new Response(JSON.stringify({ message: '测试成功', url }), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
